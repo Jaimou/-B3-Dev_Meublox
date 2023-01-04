5ae93acfacd5ecead9ff198e20c5848c2bf3658c
Etienne Decourselle <decourselle.etienne@hotmail.fr>
2023-01-04 22:41:24 +0100

    login page

--- frontend/src/components/facebook_login/FacebookLog.jsx
+++ frontend/src/components/facebook_login/FacebookLog.jsx
@@ -1,35 +1,57 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import FacebookLogin from 'react-facebook-login';
+import './FacebookLog.scss'
 
-const FacebookLog = () => {
+export default class FacebookLog extends Component {
+    state = {
+        isLoggedIn: false,
+        userID: "",
+        name: "",
+        email: "",
+        picture: ""
+    };
 
-
-    const [login, setLogin] = useState(false);
-    const [data, setData] = useState({});
-    const [picture, setPicture] = useState('');
-
-
-    const responseFacebook = (response) => {
+    responseFacebook = (response) => {
         console.log(response);
-        setData(response);
-        setPicture(response.picture.data.url);
-        if (response.accessToken) {
-            setLogin(true);
-        } else {
-            setLogin(false);
+        if (response.status !== "unknown") {
+            this.setState({
+                isLoggedIn: true,
+                userID: response.userID,
+                name: response.name,
+                email: response.email,
+                picture: response.picture.data.url
+            });
         }
+    };
+
+    componentClicked = () => {
+        console.log("clicked");
+    };
+
+    render() {
+        const { email, isLoggedIn, name, picture } = this.state;
+        let fbContent;
+
+        if (isLoggedIn) {
+            fbContent = (
+                <div>
+                    <img src={picture} alt={name} />
+                    <h2>Welcome {name}</h2>
+                    Email: {email}
+                </div>
+            );
+        } else {
+            fbContent = (
+                <FacebookLogin
+                    appId="2193887884135559"
+                    autoLoad={true}
+                    fields="name,email,picture"
+                    onClick={this.componentClicked}
+                    callback={this.responseFacebook}
+                    icon="fa-facebook"
+                />
+            );
+        }
+        return <div>{fbContent}</div>;
     }
-
-
-    return (
-        <div>
-            {!login}
-            {login}
-
-            {data.name}
-            {data.email}
-
-        </div>
-    );
 }
-export default FacebookLog
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
